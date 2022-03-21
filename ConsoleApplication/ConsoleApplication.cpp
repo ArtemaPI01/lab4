b4634164a7489e32a98d0e5ee83eb2b46a73e86f
ArtemaPI01 <klimaa22@yandex.ru>
2022-03-22 01:27:07 +0700

    4

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include "locale.h"
 #include <stdio.h>
 #include <conio.h>
@@ -7,6 +8,7 @@
 #include "Shop.h"
 #include "Owner.h"
 #include "Boxoffice.h"
+#include "Warehouse.h"
 #include <string>
 
 using namespace std;
@@ -18,6 +20,7 @@ int main()
 	Shop* Ashan;
 	Owner* Alisher;
 	Boxoffice* b;
+	Warehouse* a;
 	Ashan = new Shop[1];
 	for (int i = 0; i < 1; i++)
 	{
@@ -30,9 +33,17 @@ int main()
 	Alisher->enter();
 	Alisher->print();
 	delete Alisher;
+	a = new Warehouse;
+	(*a).init("Арбуз Тыква Огурец", 7);
+	(*a).print();
+	int t = 20;
+	std::cout << "\nСкорость разгрузки "<< t <<" тонн в минутах: " << (*a).cloakWarehouse(20) << "\n";
+	(*a).Poisk("Тыква");
+	delete a;
 	b = new Boxoffice;
 	b->enter();
 	b->print();
 	b->cloakBoxoffice(100);
 	delete b;
+	_getch;
 }
