da2fb04f9224dbfcae7ea868e34fb76ea36cf29f
ArtemaPI01 <klimaa22@yandex.ru>
2022-03-22 01:16:47 +0700

    3

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -4,7 +4,9 @@
 #include <conio.h>
 #include <iostream>
 #include <Windows.h>
+#include "Shop.h"
 #include "Owner.h"
+#include "Boxoffice.h"
 #include <string>
 
 using namespace std;
@@ -13,9 +15,24 @@ int main()
 {
 	setlocale(LC_ALL, "Rus");
 	SetConsoleCP(1251);
+	Shop* Ashan;
 	Owner* Alisher;
+	Boxoffice* b;
+	Ashan = new Shop[1];
+	for (int i = 0; i < 1; i++)
+	{
+		Ashan[i].enter();
+		Ashan[i].print();
+		Ashan[i].cloakShop();
+	}
+	delete[] Ashan;
 	Alisher = new Owner;
 	Alisher->enter();
 	Alisher->print();
 	delete Alisher;
+	b = new Boxoffice;
+	b->enter();
+	b->print();
+	b->cloakBoxoffice(100);
+	delete b;
 }
